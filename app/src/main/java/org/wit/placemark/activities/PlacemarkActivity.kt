9b36585876a684b6ac91f5669787ffdcb09cacf9
Oleksandr Kononov <sashakon13@gmail.com>
2018-09-24 12:56:32 +0100

    Lab02

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -2,37 +2,59 @@ package org.wit.placemark.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_placemark.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
-  val placemark = PlacemarkModel()
-  val placemarks = ArrayList<PlacemarkModel>()
+  var placemark = PlacemarkModel()
+  lateinit var app : MainApp
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
-    info("Placemark Activity started..")
+    app = application as MainApp
+
+    toolbarAdd.title = title
+    setSupportActionBar(toolbarAdd)
 
     btnAdd.setOnClickListener() {
       placemark.title = placemarkTitle.text.toString()
       placemark.description = placemarkDescription.text.toString()
-      if (placemark.title.isNotEmpty() && placemark.description.isNotEmpty()) {
-        info("add Button Pressed: ${placemark.title} ${placemark.description}")
-        placemarks.add(placemark)
-        placemarkTitle.setText("")
-        placemarkDescription.setText("")
+      if (placemark.title.isNotEmpty()) {
+        app.placemarks.add(placemark.copy())
+        info("add Button Pressed: $placemarkTitle")
+        app.placemarks.forEach { info("add Button Pressed: ${it}")}
         toast("Successfully added!")
+        setResult(AppCompatActivity.RESULT_OK)
+        finish()
       }
       else {
-        toast("Please Enter a title and description")
+        toast("Please Enter a title")
       }
     }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_placemark, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> {
+        setResult(AppCompatActivity.RESULT_OK)
+        finish()
+      }
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
